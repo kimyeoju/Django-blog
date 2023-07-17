e6a874b8ebe9ab6ee2dba034256b8887587f9d49
kimyeoju <yeojoo0031@gmail.com>
2023-07-18 00:26:17 +0900

    update수정

--- projects/blog/urls.py
+++ projects/blog/urls.py
@@ -6,10 +6,13 @@ app_name = 'blog'
 urlpatterns = [
     # 글 목록 조회
     path('',views.Index.as_view(), name='list'),
+    
     # 글 작성
     path('write/',views.Write.as_view(), name='write'),
+    
     # 글 상세 조회
     path('detail/<int:post_id>/', views.DetailView.as_view(), name='detail'),
+    
     # 글 수정
     path('detail/<int:post_id>/edit/', views.Update.as_view(), name='edit'),
 ]
--- projects/blog/views.py
+++ projects/blog/views.py
@@ -33,7 +33,7 @@ class Write(View):
 
 class DetailView(View):
     def get(self, request, post_id):
-        post = Post.objects.get(pk=post_id)
+        post = Post.objects.get(id=post_id)
         context = {
             'title': 'Blog',
             'post_id': post_id,
@@ -47,7 +47,7 @@ class DetailView(View):
 
 class Update(View):
     def get(self,request, post_id):
-        post = get_object_or_404(Post, pk=post_id)
+        post = get_object_or_404(Post, id=post_id)
         # 조건을 만족하는 object가 있으면 해당 object를 리턴하고, 없으면 404처리 첫번째 인자 Post에서 두번째 인자 해당 아이디의 post객체를 가져오는 것 
         form = PostForm(initial={'title': post.title, 'content': post.content})
         context = {
@@ -59,18 +59,20 @@ class Update(View):
         return render(request, 'blog/post_edit.html',context)
     
     def post(self,request, post_id):
-        post = get_object_or_404(Post, pk=post_id)
+        post = get_object_or_404(Post, id=post_id)
         form = PostForm(request.POST)
+        
         if form.is_valid():
             # 폼의 입력값을 얻고 싶은 경우는 form.cleaned_data
             # form.cleaned_data로 접근하면 폼 인스턴스 내에서 clean함수를 통해 변환되었을 수도 있는 데이터를 가져오므로 좋은 접근
             post.title = form.cleaned_data['title']
             post.content = form.cleaned_data['content']
             post.save()
-            return redirect('blog:detail', pk=post_id)
+            return redirect('blog:detail', post_id=post_id)
         
         context = {
-        'form': form,
-        'title': 'Blog',
-        } 
+            'form': form,
+            'title': 'Blog',
+            'post': post
+        }
         return render(request, 'blog/post_edit.html', context)
